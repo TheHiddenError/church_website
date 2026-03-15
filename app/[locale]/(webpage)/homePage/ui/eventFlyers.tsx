595b7a33c00306014eea61358b51e0a8d5971e7b
TheHiddenError <marcos48201@gmail.com>
2026-03-15 08:54:20 -0500

    Time change

--- app/[locale]/(webpage)/homePage/ui/eventFlyers.tsx
+++ app/[locale]/(webpage)/homePage/ui/eventFlyers.tsx
@@ -215,7 +215,7 @@ export default function EventFlyerSection(){
 
     const [index, setIndex] = useState(0);
     const [isAnimating, setIsAnimating] = useState(true);
-    const target = new Date().setHours(8,30,0,0);
+    const target = new Date().setHours(10,30,0,0);
     const [remaining, setRemaining] = useState(target - Date.now());
     const [fiveTimer, setFiveTimer] = useState(false);
     const [cooldown, setCooldown] = useState(false);
